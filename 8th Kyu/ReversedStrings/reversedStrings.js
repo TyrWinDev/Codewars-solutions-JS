ab76cda20cdcb43ef90a2553f34e55befa41095a
Pedro Medina <48919013+TyrWinDev@users.noreply.github.com>
2021-06-25 09:09:39 -0400

    Add files via upload

---
+++ 8th Kyu/ReversedStrings/reversedStrings.js
@@ -0,0 +1,9 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+
+function reverseWords(str) {
+  return str.split("").reverse().join("");
+}
+
+reverseWords("world");
